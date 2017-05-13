9a5bd26ca8c5a6b09cd15f8f7971e8690e2883e7
Jonathon Reinhart <Jonathon.Reinhart@gmail.com>
2017-05-13 02:05:57 -0400

    Add Config object and load from yaml

--- mattersmtp.py
+++ mattersmtp.py
@@ -5,6 +5,7 @@ import asyncore
 import argparse
 from email.parser import Parser
 import logging
+import yaml
 
 # Based loosely on https://github.com/kennethreitz/inbox.py
 
@@ -12,10 +13,38 @@ SMTP_PORT = 25
 
 class MatterSmtp(smtpd.SMTPServer):
     def __init__(self, address):
+        logging.info("Binding to {}".format(address))
         super().__init__(address, None)
 
     def process_message(self, peer, mailfrom, rcpttos, data):
         logging.info("Receiving message from {}".format(mailfrom))
+
+class Config:
+    def __init__(self, **kwargs):
+        self.set_defaults()
+        self.__dict__.update(kwargs)
+
+    @classmethod
+    def load_yml(cls, path):
+        """Load mattersmtp.yml"""
+        with open(path, 'r') as f:
+            data = yaml.safe_load(f) or {}
+
+        # TODO: Lots of validation of yaml data
+
+        return cls(**data)
+
+    def set_defaults(self):
+        self.loglevel = 'WARNING'
+        self.bind = dict(addr='localhost', port=25)
+
+    def merge_args(self, args):
+        if args.loglevel:
+            self.loglevel = args.loglevel
+        if args.addr:
+            self.bind['addr'] = args.addr
+        if args.port:
+            self.bind['port'] = args.port
 
 
 def parse_args():
@@ -24,22 +53,26 @@ def parse_args():
             formatter_class = argparse.ArgumentDefaultsHelpFormatter,
             )
 
-    ap.add_argument('addr', type=str, nargs='?', default='localhost',
-            help = 'addr to bind to')
-    ap.add_argument('port', type=int, nargs='?', default=SMTP_PORT,
-            help = 'port to bind to')
-    ap.add_argument('-s', '--section',
-            help =' mattersend.conf section')
+    ap.add_argument('--addr', type=str,
+            help = 'Override address to which to bind')
+    ap.add_argument('--port', type=int,
+            help = 'Override port to wich to bind')
+    ap.add_argument('-c', '--config', default='mattersmtp.yml',
+            help = 'Path to mattersmtp.yml config file')
     ap.add_argument('--loglevel', choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
-            help = 'Set the logging level (default: %(default)s)', default='WARNING')
+            help = 'Override the logging level')
     return ap.parse_args()
 
 
 def main():
     args = parse_args()
-    logging.basicConfig(level=args.loglevel)
 
-    address = (args.addr, args.port)
+    cfg = Config.load_yml(args.config)
+    cfg.merge_args(args)
+
+    logging.basicConfig(level=cfg.loglevel)
+
+    address = (cfg.bind['addr'], cfg.bind['port'])
     server = MatterSmtp(address)
 
     try:
